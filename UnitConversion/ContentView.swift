8d258e9be81ce876bc636f9daa5e24e06511ec1c
shinohara-san <someiyoshino0809@gmail.com>
2020-03-29 16:53:28 +0900

    Initial Commit

---
+++ UnitConversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConversion
+//
+//  Created by Yuki Shinohara on 2020/03/29.
+//  Copyright © 2020 Yuki Shinohara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
